0c384b7ad1a1179e8010a01cea2749b3a66c44b2
vinayakkamat <vinayak.kamat@nium.com>
2022-02-28 11:02:55 +0530

    PROFILE NAME TEST

---
+++ src/main/java/model/HrmTestData.java
@@ -0,0 +1,32 @@
+package model;
+
+public class HrmTestData {
+
+    private String username;
+    private String password;
+    private String expectedFullName;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getExpectedFullName() {
+        return expectedFullName;
+    }
+
+    public void setExpectedFullName(String expectedFullName) {
+        this.expectedFullName = expectedFullName;
+    }
+}
